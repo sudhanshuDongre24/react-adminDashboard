73530b74ce80f13fbcb2c62af3aaec62277becd5
sudhanshuDongre24 <sudhanshuDongre24@gmail.com>
2023-09-03 09:49:31 +0530

    Navbar and footer added

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,7 +2,12 @@ import React from "react";
 import "./footer.scss";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className="footer">
+      <span>Iamadmin</span>
+      <span>&copy; Iamadmin admin dashboard</span>
+    </div>
+  );
 };
 
 export default Footer;
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,7 +2,32 @@ import React from "react";
 import "./navbar.scss";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div className="navbar">
+      <div className="logo">
+        <img src="/src/assets/logo.svg" alt="" />
+        <span>Iamadmin</span>
+      </div>
+      <div className="icons">
+        <img src="/src/assets/search.svg" alt="" className="icon" />
+        <img src="/src/assets/app.svg" alt="" className="icon" />
+        <img src="/src/assets/expand.svg" alt="" className="icon" />
+        <div className="notification">
+          <img src="/src/assets/notifications.svg" alt="" className="icon" />
+          <span>1</span>
+        </div>
+        <div className="user">
+          <img
+            src="https://images.pexels.com/photos/4050023/pexels-photo-4050023.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+            alt=""
+            className="icon"
+          />
+          <span>Sam</span>
+        </div>
+        <img src="/src/assets/settings.svg" alt="" />
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
